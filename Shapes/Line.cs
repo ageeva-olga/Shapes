5aa5447e5b3a4feb18f1f2e4a7739a185cbeeeb1
Olga Ageeva <olga.ageeva.999@mail.ru>
2019-08-25 14:46:49 +0300

    Добавила нехватающую фигурную скобку

--- Shapes/Line.cs
+++ Shapes/Line.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace Shapes
 {
-    public class Line:ILength
+    public class Line : ILength
     {
         public Point StartPoint { get; }
         public Point EndPoint { get; }
-        public Line (Point point1, Point point2)
+        public Line(Point point1, Point point2)
         {
             StartPoint = point1;
             EndPoint = point2;
@@ -22,5 +22,6 @@ namespace Shapes
                 return Math.Sqrt(Math.Pow((EndPoint.X - StartPoint.X), 2)
                                  + Math.Pow((EndPoint.Y - StartPoint.Y), 2));
             }
+        }
     }
 }
